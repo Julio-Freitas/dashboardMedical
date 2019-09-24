43284f06993fcf66e2cfb4840478fd1c15125707
Julio Freitas <julio.clf31@gmail.com>
2019-09-23 22:06:16 -0300

    Subindo App de dashboard

---
+++ js/script.js
@@ -0,0 +1,561 @@
+/** Arry com os valores que serão renderizados na tela*/
+var data = [{
+    id: 1,
+    mes: 'Jun-19',
+    vinculo: 'PF',
+    fonte: 'Hospital A',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 59500.0,
+    impostoPago: 9059.87,
+},
+{
+    id: 2,
+    mes: 'Jun-19',
+    vinculo: 'PF',
+    fonte: 'Hospital A',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 24500.00,
+    impostoPago: 694.70,
+},
+{
+    id: 3,
+    mes: 'Jun-19',
+    vinculo: 'PF',
+    fonte: 'Hospital A',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 7000,
+    impostoPago: 0,
+},
+{
+    id: 4,
+    mes: 'Jun-19',
+    vinculo: 'PF',
+    fonte: 'Hospital A',
+    tipofonte: 'Particular',
+    rendimentoBruto: 17500,
+    impostoPago: 0,
+},
+{
+    id: 5,
+    mes: 'Jun-19',
+    vinculo: 'PJ',
+    fonte: 'Hospital C',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 49000.00,
+    impostoPago: 7021.70,
+},
+{
+    id: 6,
+    mes: 'Jun-19',
+    vinculo: 'PJ',
+    fonte: 'Plano Saúde C',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 20300.00,
+    impostoPago: 2908.99,
+},
+{
+    id: 7,
+    mes: 'Jun-19',
+    vinculo: 'PJ',
+    fonte: 'Plano Saúde D',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 19600,
+    impostoPago: 2808.68,
+},
+{
+    id: 8,
+    mes: 'Jun-19',
+    vinculo: 'PJ',
+    fonte: 'Plano Saúde E',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 8400,
+    impostoPago: 1203.72,
+}, {
+    id: 9,
+    mes: 'Jun-19',
+    vinculo: 'PJ',
+    fonte: 'Consultório',
+    tipofonte: 'Partcular',
+    rendimentoBruto: 38500,
+    impostoPago: 5517.05,
+},
+
+
+/** Julho  */
+{
+    id: 10,
+    mes: 'Jul-19',
+    vinculo: 'PF',
+    fonte: 'Hospital A',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 72250.0,
+    impostoPago: 11001.27,
+},
+{
+    id: 11,
+    mes: 'Jul-19',
+    vinculo: 'PF',
+    fonte: 'Hospital A',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 29750.00,
+    impostoPago: 843.57,
+},
+{
+    id: 12,
+    mes: 'Jul-19',
+    vinculo: 'PF',
+    fonte: 'Hospital A',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 8500,
+    impostoPago: 0,
+},
+{
+    id: 13,
+    mes: 'Jul-19',
+    vinculo: 'PF',
+    fonte: 'Hospital A',
+    tipofonte: 'Particular',
+    rendimentoBruto: 21250.00,
+    impostoPago: 0,
+},
+{
+    id: 14,
+    mes: 'Jul-19',
+    vinculo: 'PJ',
+    fonte: 'Hospital C',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 59500.00,
+    impostoPago: 8526.35,
+},
+{
+    id: 15,
+    mes: 'Jul-19',
+    vinculo: 'PJ',
+    fonte: 'Plano Saúde C',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 24650,
+    impostoPago: 23532.345,
+},
+{
+    id: 16,
+    mes: 'Jul-19',
+    vinculo: 'PJ',
+    fonte: 'Plano Saúde D',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 23800,
+    impostoPago: 3410.54,
+},
+{
+    id: 17,
+    mes: 'Jul-19',
+    vinculo: 'PJ',
+    fonte: 'Plano Saúde E',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 10200,
+    impostoPago: 1461.60,
+}, {
+    id: 18,
+    mes: 'Jul-19',
+    vinculo: 'PJ',
+    fonte: 'Consultório',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 46750,
+    impostoPago: 6699.27,
+},
+
+/** Agosto  */
+{
+    id: 19,
+    mes: 'Ago-19',
+    vinculo: 'PF',
+    fonte: 'Hospital A',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 85000.0,
+    impostoPago: 12942.67,
+},
+{
+    id: 20,
+    mes: 'Ago-19',
+    vinculo: 'PF',
+    fonte: 'Hospital A',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 35000.00,
+    impostoPago: 992.43,
+},
+{
+    id: 21,
+    mes: 'Ago-19',
+    vinculo: 'PF',
+    fonte: 'Hospital A',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 10000,
+    impostoPago: 0,
+},
+{
+    id: 13,
+    mes: 'Ago-19',
+    vinculo: 'PF',
+    fonte: 'Hospital A',
+    tipofonte: 'Particular',
+    rendimentoBruto: 25000.00,
+    impostoPago: 0,
+},
+{
+    id: 14,
+    mes: 'Ago-19',
+    vinculo: 'PJ',
+    fonte: 'Hospital C',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 70000.00,
+    impostoPago: 10031.00,
+},
+{
+    id: 15,
+    mes: 'Ago-19',
+    vinculo: 'PJ',
+    fonte: 'Plano Saúde C',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 29000,
+    impostoPago: 4155.70,
+},
+{
+    id: 16,
+    mes: 'Ago-19',
+    vinculo: 'PJ',
+    fonte: 'Plano Saúde D',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 28000,
+    impostoPago: 4012.40,
+},
+{
+    id: 17,
+    mes: 'Ago-19',
+    vinculo: 'PJ',
+    fonte: 'Plano Saúde E',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 12000,
+    impostoPago: 1719.60,
+}, {
+    id: 18,
+    mes: 'Ago-19',
+    vinculo: 'PJ',
+    fonte: 'Consultório',
+    tipofonte: 'Hospital/Convênios',
+    rendimentoBruto: 55000,
+    impostoPago: 7881.50,
+},
+
+]
+
+
+
+
+/** valor ideal de alocação   elemento HTML  */
+
+let filterEl = document.querySelector('#filter');
+var valorIdealEl = document.querySelector('.alocacao-ideal #valorIdeal');
+var valorRealEl = document.querySelector('.alocacao-real #valorReal');
+
+var previIdealEl = document.querySelector('.previ-ideal #valorIdeal');
+var previRealEl = document.querySelector('.previ-real span#valorReal');
+var rendTotal;
+
+var previReal;
+var previIdeal;
+
+
+/** iniiando a tabela a partir do último mês */
+var valor = filtro('Ago-19');
+var arrRendiBruto = [];
+var arrPrevi = [];
+
+/** funções */
+dadosEl();
+chartRend();
+
+/** mudando dados na tela de acorodo com o valor do select */
+filterEl.addEventListener('change', e => {
+    let dados = document.querySelector('#dados');
+
+    dados.innerHTML = '';
+
+    valor = filtro(e.target.value);
+    dadosEl();
+
+    valor.filter(item => {
+        arrRendiBruto.push(item.rendimentoBruto);
+        
+        if(item.vinculo === 'PF') {
+            arrPrevi.push(item.rendimentoBruto);
+        }
+    });
+
+
+    somaRendimento();
+    somaPrevi();
+    
+
+    verificaAloc(e.target.value, 30, rendTotal);
+    chartPrev(previIdeal, previReal);
+
+});
+
+
+/** criando tabela com os valores */
+function dadosEl() {
+    valor.forEach(item => {
+        valorIdealEl.innerHTML = "";
+        let tr = document.createElement('tr');
+        let id = document.createElement('td');
+        let vinculo = document.createElement('td');
+        let fonte = document.createElement('td');
+        let tipofonte = document.createElement('td');
+        let rendimentoBruto = document.createElement('td');
+        let impostoPago = document.createElement('td');
+        let aliquota = document.createElement('td');
+
+        let aliquotaMedia = (item.impostoPago / item.rendimentoBruto) * 100;
+
+        let valueRendimento = formatMoeda(item.rendimentoBruto);
+        let valueimposto = formatMoeda(item.impostoPago);
+
+        id.innerHTML = item.id;
+        vinculo.innerHTML = item.vinculo;
+        fonte.innerHTML = item.fonte;
+        tipofonte.innerHTML = item.tipofonte;
+        rendimentoBruto.innerHTML = `R$ ${valueRendimento}`;
+        rendimentoBruto.classList.add('rendimentoBruto')
+        impostoPago.innerHTML = `R$ ${valueimposto}`;
+        aliquota.innerHTML = `${aliquotaMedia.toFixed(2)} %`;
+
+        tr.appendChild(id);
+        tr.appendChild(vinculo);
+        tr.appendChild(fonte);
+        tr.appendChild(tipofonte);
+        tr.appendChild(rendimentoBruto);
+        tr.appendChild(impostoPago);
+        tr.appendChild(aliquota);
+
+        dados.appendChild(tr);
+
+        arrRendiBruto.push(item.rendimentoBruto);
+
+   
+        switch(item.vinculo){
+            case 'PF':
+            arrPrevi.push(item.rendimentoBruto);
+            break;
+        }
+       
+        
+        somaRendimento();
+        somaPrevi();
+       
+
+    });
+
+    
+    verificaAloc('Ago-19');
+    chartAloc(30, (60000.00 / 349000) * 100);
+    chartPrev(5115, 2887.50)
+   
+}   
+
+
+/** filtranndo mês */
+function filtro(valor) {
+    return data.filter(el => {
+        let response = el.mes === valor;
+        return response;
+    });
+}
+
+/** calculando rendimento bruto */
+function somaRendimento() {
+    valorIdealEl.innerHTML = "";
+    let somaRend = arrRendiBruto.reduce((total, valor) => {
+        let soma = total + valor;
+        return soma;
+    }, 0);
+    let valorIdeal = somaRend * 0.3;
+    
+    rendTotal = somaRend;
+    rendimentoBrutoTotal = rendTotal;
+    valorIdealEl.innerHTML = `R$ ${valorIdeal.toLocaleString('pt-BR')}`;
+
+    arrRendiBruto = [];
+    
+}
+
+
+function somaPrevi() {
+    previIdealEl.innerHTML = "";
+    let somaRend =  arrPrevi.reduce((total, valor) => {
+        let soma = total + valor;
+        return soma;
+    }, 0);
+    let valorIdeal = somaRend * 0.12;
+
+    previIdeal = valorIdeal * 0.275;
+    rendTotal = somaRend;
+    previIdealEl.innerHTML = `R$ ${valorIdeal.toLocaleString('pt-BR')}`;
+    arrPrevi = [];
+    
+}
+
+/** formatando moeda */
+function formatMoeda(valor) {
+    return valor.toLocaleString('pt-BR', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+}
+
+/** verificando mes para setar valor de alocação real */
+function verificaAloc(value, ideal, real) {
+    ideal = 30;
+    switch (value) {
+        case 'Jun-19':
+            previReal = (7500 * 0.275);
+            valorReal = 42000.00;
+            chartAloc(ideal, (valorReal / real) * 100);
+            valorRealEl.innerHTML = `R$ ${valorReal.toLocaleString('pt-BR')}`;
+            previRealEl.innerHTML = `R$ ${previReal.toLocaleString('pt-BR')}`;
+          
+            break;
+        case 'Jul-19':
+            previReal = (9000 * 0.275);
+            valorReal = 51000.00;
+            chartAloc(ideal, (valorReal / real) * 100);
+            valorRealEl.innerHTML = `R$ ${valorReal.toLocaleString('pt-BR')}`;
+            previRealEl.innerHTML = `R$ ${previReal.toLocaleString('pt-BR')}`;
+            break;
+        case 'Ago-19':
+            valorReal = 60000.00;
+            previReal = (10500 * 0.275);
+            chartAloc(ideal, (valorReal / real) * 100);
+           
+            valorRealEl.innerHTML = `R$ ${valorReal.toLocaleString('pt-BR')}`;
+            previRealEl.innerHTML = `R$ ${previReal.toLocaleString('pt-BR')}`;
+            break;
+        default:
+            chartAloc(ideal, (valorReal / real) * 100);
+            valorRealEl.innerHTML = 0;
+    }
+}
+
+
+/***  chart.js */
+function chartAloc(ideal, real) {
+    var ctx = document.getElementById('alocData');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'pie',
+        // The data for our dataset
+        data: {
+            labels: ['Ideal %', 'Real %'],
+            datasets: [{
+                data: [ideal, real],
+                backgroundColor: ['rgb(54,162,235)', '#FFC857']
+            }],
+        },options: {
+            title: {
+                display: true,
+                text: 'Alocação ideal e real',
+                fontSize: 18,
+            }
+        }
+    });
+
+}
+
+
+function chartPrev(ideal, real) {
+    var ctx = document.getElementById('previData');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Economia Ideal', 'Economia real'],
+          datasets: [{
+            data:[ideal.toFixed(2), real.toFixed(2)],
+            label: ['Economia Ideal', 'Economia real'],
+            backgroundColor: ['rgb(54,162,235)', '#FFC857'],
+            boderColor: 'rgb(54,160,35)',
+            fill: true,
+            pointBackgroundColor: '#fff'
+          }]
+        }, 
+          options: {
+            tooltips: {
+              mode: 'index',
+              intersect: false
+            }, 
+            scales: {
+              yAxes:[{
+                display: true,
+                scaleLabel: {
+                  display: true,
+                  labelString: 'Valores em R$'
+                }
+              }],
+            }, 
+            title: {
+                display: true,
+                text: 'Previdência',
+                fontSize: 18
+            },
+            legend: {
+                display: false,
+            }      
+          }
+
+    });
+
+}
+
+
+function chartRend(ideal, real) {
+    var ctx = document.getElementById('rendimentoTotal');
+    var chart = new Chart(ctx,{
+        type:"line",
+        data:{
+            labels:["Jun-2019","Jul-19","Ago-19"],
+            datasets:[{
+                label:"Rendimento Total R$",
+                data:[108500,131750,155000],
+                fill:true,
+                borderColor:"rgb(54,162,235)",
+                backgroundColor: "#D2E4EF"
+            }]
+        }, 
+        options: {
+            title: {
+                display: true,
+                text: 'Rendimento Bruto',
+                fontSize: 18
+            },
+            legend: {
+                display: false,
+               
+            },
+            scales: {
+                yAxes:[{
+                  display: true,
+                  scaleLabel: {
+                    display: true,
+                    labelString: 'Valores em R$',
+                    stacked: true
+                  }
+                }],
+                ticks: {
+                    suggestedMin: 50,
+                    suggestedMax: 100
+                }
+              }
+        }
+    
+    });
+
+}
+
+
